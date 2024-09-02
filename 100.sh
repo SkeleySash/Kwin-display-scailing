d124da99d856f8f75e9c59e820fdb26e889db599
HoleProf <59277987+HoleProfed@users.noreply.github.com>
2024-09-02 15:17:29 +1000

    Update 100.sh

--- 100.sh
+++ 100.sh
@@ -4,7 +4,7 @@ current_scale=$(kscreen-doctor -o | grep "Scale:" | awk '{print $2}')
 
 if [ "$current_scale" = "1" ]; then
     kscreen-doctor output.1.scale.1
-    notify-send "Display Scaling" "Scaled to 1100%"
+    notify-send "Display Scaling" "Scaled to 100%"
 else
     kscreen-doctor output.1.scale.1
     notify-send "Display Scaling" "Scaled to 100%"
